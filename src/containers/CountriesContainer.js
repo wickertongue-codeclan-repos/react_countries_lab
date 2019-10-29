9ad0fa4680028dc52177ef7082e5e29e90be53d3
hayden <haydenwb@gmail.com>
2019-10-29 13:27:43 +0000

    set up files, added small edits to CountriesContainer

---
+++ src/containers/CountriesContainer.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import CountriesList from '../components/CountriesList';
+import CountryDetail from '../components/CountryDetail';
+
+class CountriesContainer extends Component {
+  // constructor
+    // state (api)
+    // props (countries)
+}
